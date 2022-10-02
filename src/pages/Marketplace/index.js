c89d446c63b39f29046bfcae63edcf7041a2aeb6
gyogui <yoguigeraldo@yahoo.com.br>
2022-10-02 18:33:34 -0300

    Recursos do Painel. Modal e Array, Renderizar entradas na lista de avisos

--- src/pages/Marketplace/index.js
+++ src/pages/Marketplace/index.js
@@ -2,15 +2,50 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Marketplace() {
+export default function Main() {
     return (
-        <View>
-            <Text>Marketplace</Text>
+
+        <View style={styles.container}>
+            <View style={styles.containerTitle}>
+                <Text style={styles.titleHome}>MarketPlace</Text>
+            </View>
+            <View style={styles.containerBody}>
+            </View>
         </View>
+
     );
 }
 
-container:{
+const styles = StyleSheet.create({
+    
+    container:{
+        flex: 1,
+        backgroundColor: '#f6f6ed',
+        marginTop:60,
+    },
+    containerTitle:{
+        flex:1,
+        fontWeight:'bold',
+    },
+    containerBody:{
+        flex:10,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#a6c6ff',
+        borderBottomLeftRadius:25,
+        borderBottomRightRadius:25,
+        borderTopLeftRadius:25,
+        borderTopRightRadius:25,
+        paddingStart:10,
+        paddingEnd:10,
+    },
+    titleHome:{
+        fontSize:30,
+        textAlign:'center',
+    },
+    textBody:{
+        fontSize: 25,
 
 
-}
+    }
+})
